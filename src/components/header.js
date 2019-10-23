7e3d655297374db67c40d6f1df73d3c72472bb2f
at1208 <amantiwari12081999@gmail.com>
2019-10-23 18:48:38 +0530

    LOGIN and SIGNUP button added on header section

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import './header.css'
 
 const Header = () => {
-  return <div>
-
+  return <div className='container-fluid'>
+       <div className='a1'>
+       <button className='float-right btn btn-md a2'>SIGN UP</button>
+       <button className='float-right btn btn-md a2'>LOGIN</button>
+       </div>
          </div>
 }
 export default Header;
