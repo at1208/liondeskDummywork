d1315d58099a9a7cb646ba8008eb280c292c002c
at1208 <amantiwari12081999@gmail.com>
2019-10-23 19:23:16 +0530

    dashboard component created

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './login.css';
+import { Link } from 'react-router-dom'
+import Header from '../header'
 
 const Login  = () => {
-  return <div className='container'>
+  return <div className='container-fluid'>
+  <Header />
   <div className='card container b1 col-md-5'>
-        <h1 className='text-center'>LOGIN</h1>
+         <h1 className='text-center'>LOGIN</h1>
           <label>Username</label>
           <input className='b3 text-center' placeholder='Please Enter your Username'/>
 
           <label>Password</label>
           <input className='b3 text-center'placeholder='Please Enter your Password'/>
 
-          <button className='btn btn-sm b2  '>Submit</button>
+          <Link to='/dashboard'><button className='btn btn-block b2 '>Submit</button></Link>
   </div>
 </div>
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route} from 'react-router-dom'
 import App from './components/app';
 import Login from './components/Login/login'
+import Dashboard from './components/Dashboard/dashboard'
 
 
 ReactDOM.render(
    <BrowserRouter>
-     <Route path='/' component={App} />
+     <Route path='/' exact component={App} />
      <Route path='/login' component={Login} />
+     <Route path='/dashboard'   component={Dashboard} />
    </BrowserRouter>, document.getElementById('root'));
