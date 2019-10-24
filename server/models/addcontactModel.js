8a3274d8578aa19521d56c5bf624e8661eee0888
at1208 <amantiwari12081999@gmail.com>
2019-10-24 14:11:46 +0530

    mongoose schema  validation added

--- server/models/addcontactModel.js
+++ server/models/addcontactModel.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 const AddContactSchema = new mongoose.Schema({
-  _id: Number,
-  name: String,
-  ph: Number,
+  _id:  { type: Number, required: true },
+  name: { type: String, required: true },
+  ph: { type: Number, required: true },
 })
 mongoose.model('Contacts',AddContactSchema)
